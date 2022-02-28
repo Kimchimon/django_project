22f5af3c544a3debe12d47f1cd60574f8d490f7c
Yoon <wjddbscls@naver.com>
2022-02-28 16:49:08 +0900

    add DetailView

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,16 +3,14 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
@@ -35,3 +33,10 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     # 회원가입시 보여주는 html
     template_name = 'create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    # 상대편의 정보를 보여주도록 설정
+    context_object_name = 'target_user'
+    template_name = 'detail.html'
