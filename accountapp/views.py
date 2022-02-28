469e12710641620ed8407faafacff2065ff2e0b0
Yoon <wjddbscls@naver.com>
2022-03-01 02:55:57 +0900

    ready to build profile

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_reqired
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
+has_ownership = [login_required,account_ownership_reqired]
+
+@login_required
 def hello_world(request):
     if request.method == "POST":
 
@@ -40,3 +47,25 @@ class AccountDetailView(DetailView):
     # 상대편의 정보를 보여주도록 설정
     context_object_name = 'target_user'
     template_name = 'detail.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(UpdateView):
+    model = User
+    context_object_name = 'target_user'
+    # 기본적인 Form을 제공해줌
+    form_class = AccountUpdateForm
+    # 업데이트 성공시 리턴할 html
+    success_url = reverse_lazy('accountapp:hello_world')
+    # 업데이트시 보여주는 html
+    template_name = 'create.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'delete.html'
