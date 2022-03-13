eec687514094b8e4a77a2f25ad09147a86378324
Yoon <wjddbscls@naver.com>
2022-03-14 03:18:40 +0900

    build comment

--- jango/settings.py
+++ jango/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'profileapp',
+    'commentapp',
     'accountapp',
     'articleapp',
 ]
--- profileapp/urls.py
+++ profileapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'profileapp'
 
 urlpatterns = [
     path('create/', ProfileCreateView.as_view(), name='create'),
-    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'), # 어떤 프로파일에 접근해야되는지 지정해줘야 하기 때문에 pk 지정해줌
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),  # 어떤 프로파일에 접근해야되는지 지정해줘야 하기 때문에 pk 지정해줌
 ]
